4af4b49280ce82ea805e13b48262af40a5eab82c
yamadagennji <waynezhiyong@gmail.com>
2017-01-03 19:59:27 -0500

    Create 88. Merge Sorted Array.py

---
+++ 88. Merge Sorted Array.py
@@ -0,0 +1,17 @@
+#别璐莎想出来的
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        while True:
+            if len(nums1)>m:
+                nums1.pop(len(nums1)-1)
+            else:
+                break
+        nums1.extend(nums2)
+        nums1.sort() 
