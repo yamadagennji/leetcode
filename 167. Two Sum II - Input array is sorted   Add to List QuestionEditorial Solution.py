1b69cc1a54bf2401990d7183b56384029b5b2aaa
yamadagennji <waynezhiyong@gmail.com>
2017-01-04 16:08:24 -0500

    Update 167. Two Sum II - Input array is sorted   Add to List QuestionEditorial Solution.py

--- 167. Two Sum II - Input array is sorted   Add to List QuestionEditorial Solution.py
+++ 167. Two Sum II - Input array is sorted   Add to List QuestionEditorial Solution.py
@@ -13,3 +13,30 @@ class Solution(object):
             dic[num] = i#将num保存在i位置上
         
         
+# binary search        
+def twoSum(self, numbers, target):
+    for i in xrange(len(numbers)):
+        l, r = i+1, len(numbers)-1
+        tmp = target - numbers[i]
+        while l <= r:
+            mid = l + (r-l)//2
+            if numbers[mid] == tmp:
+                return [i+1, mid+1]
+            elif numbers[mid] < tmp:
+                l = mid+1
+            else:
+                r = mid-1
+                
+                
+                
+# two-pointer
+def twoSum1(self, numbers, target):
+    l, r = 0, len(numbers)-1
+    while l < r:
+        s = numbers[l] + numbers[r]
+        if s == target:
+            return [l+1, r+1]
+        elif s < target:
+            l += 1
+        else:
+            r -= 1
